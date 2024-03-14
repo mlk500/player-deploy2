2e5a350ab202a22c1888bc08aae8b19bc9b7775a
Malak_17 <57171298+mlk500@users.noreply.github.com>
2024-03-14 05:28:15 +0200

    no dups camera

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "277b8783ffcb15f2000813047ea4f135",
-"index.html": "c5694384f5d7b1f76c24f6cddd1876bc",
-"/": "c5694384f5d7b1f76c24f6cddd1876bc",
-"main.dart.js": "5040d9c3aa3f7e336d41a0ae2ad450e6",
+"index.html": "a4db462917312f7e9611ac9c0e93771e",
+"/": "a4db462917312f7e9611ac9c0e93771e",
+"main.dart.js": "1dd335cb18c159b1f2dd99a14c5701e7",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
